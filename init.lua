e77ec3b51e9bb60a7f7c9f2818a79531c4a72b2e
Youssif Salama <youssifsalama@gmail.com>
2024-10-17 12:34:26 -0400

    Add copy paste support

--- init.lua
+++ init.lua
@@ -519,6 +519,19 @@ lspconfig.emmet_ls.setup({
   filetypes = { "html", "css", "eelixir", "heex" },
 })
 
+-- Force copy pasting support using OSC52
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
